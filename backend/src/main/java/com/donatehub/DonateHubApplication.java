8b559707d3db37648baa8f5201f7751717fd629e
Chayame-Barhdadi <maeshay6@gmail.com>
2026-04-25 21:02:44 +0100

    chore: initialize project structure

---
+++ backend/src/main/java/com/donatehub/DonateHubApplication.java
@@ -0,0 +1,13 @@
+package com.donatehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonateHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonateHubApplication.class, args);
+	}
+
+}
